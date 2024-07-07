5b11a82895f58d3504840605ce42052662a062be
Jason Newland <ninja.jdn@gmail.com>
2024-07-07 13:33:58 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-javascript
     function analyzeDeck() {
         // Placeholder function for analyzing the deck
         document.getElementById('result').innerText = 'Deck analysis will be shown here.';
